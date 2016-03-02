1ba3da8871d7336e540471f38b3174991b51140c
boltex <fmalboeuf@oceanex.com>
2016-03-02 16:56:31 -0500

    Initial commit to test fusioncharts in adf

---
+++ src/fusiontest.js
@@ -0,0 +1,47 @@
+'use strict';
+
+angular.module('adf.widget.fusiontest', ['adf.provider', 'ng-fusioncharts'])
+  .config(function(dashboardProvider){
+    dashboardProvider
+      .widget('fusiontest', {
+        title: 'Fusion Test',
+        description: 'Widget test with Fusion Graph',
+        templateUrl: '{widgetsPath}/fusiontest/src/view.html',
+        controller:  'fusiontestViewController',
+        edit: {
+          templateUrl: '{widgetsPath}/fusiontest/src/edit.html'
+        }
+      });
+  }).controller('fusiontestViewController', function($scope){
+
+    $scope.datasource = {
+      chart: {
+        caption: "Harry's SuperMart",
+        subCaption: "Top 5 stores in last month by revenue",
+        numberPrefix: "$",
+        theme: "ocean"
+      },
+      data:[{
+        label: "Bakersfield Central",
+        value: "880000"
+      },
+      {
+        label: "Garden Groove harbour",
+        value: "730000"
+      },
+      {
+        label: "Los Angeles Topanga",
+        value: "590000"
+      },
+      {
+        label: "Compton-Rancho Dom",
+        value: "520000"
+      },
+      {
+        label: "Daly City Serramonte",
+        value: "330000"
+      }]
+    };
+
+
+  });
